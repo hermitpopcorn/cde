a95f25a874081d3086ba46419ef1665b97cc4a8a
Dani Nugraha <dani.hmng@gmail.com>
2022-05-17 20:45:04 +0700

    changed indentation
    also added editorconfig

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,6 @@
 #![cfg_attr(
-  all(not(debug_assertions), target_os = "windows"),
-  windows_subsystem = "windows"
+	all(not(debug_assertions), target_os = "windows"),
+	windows_subsystem = "windows"
 )]
 
 const DATABASE_NAME: &str = "datent";
@@ -15,77 +15,77 @@ static DB: OnceCell<Database> = OnceCell::new();
 
 #[tauri::command]
 async fn db_connect() -> tauri::Result<bool> {
-  if DB.get().is_none() == false {
-    return Ok(false);
-  }
+	if DB.get().is_none() == false {
+		return Ok(false);
+	}
 
-  let attempt = connect_client().await;
-  match attempt {
-    Ok(client) => {
-      let _set_db = DB.set(client.database(DATABASE_NAME)).unwrap();
-      return Ok(true);
-    },
-    Err(msg) => {
-      println!("{}", msg);
-      return Err(tauri::Error::CreateWindow);
-    },
-  }
+	let attempt = connect_client().await;
+	match attempt {
+		Ok(client) => {
+			let _set_db = DB.set(client.database(DATABASE_NAME)).unwrap();
+			return Ok(true);
+		},
+		Err(msg) => {
+			println!("{}", msg);
+			return Err(tauri::Error::CreateWindow);
+		},
+	}
 }
 
 async fn connect_client() -> Result<Client, Error> {
-  let mut client_options = ClientOptions::parse("mongodb://localhost:27017").await?;
-  client_options.app_name = Some("CDE".to_string());
-  let client = Client::with_options(client_options)?;
+	let mut client_options = ClientOptions::parse("mongodb://localhost:27017").await?;
+	client_options.app_name = Some("CDE".to_string());
+	let client = Client::with_options(client_options)?;
 
-  // Attempt connection
-  match client.database("admin").run_command(doc! {"ping": 1}, None).await {
-    Ok(_) => {
-      println!("Connected to database.");
-      return Ok(client);
-    },
-    _ => {
-      return Err(anyhow!("Could not connect to database."));
-    }
-  };
+	// Attempt connection
+	match client.database("admin").run_command(doc! {"ping": 1}, None).await {
+		Ok(_) => {
+			println!("Connected to database.");
+			return Ok(client);
+		},
+		_ => {
+			return Err(anyhow!("Could not connect to database."));
+		}
+	};
 }
 
 #[allow(dead_code)]
 fn print_type_of<T>(_: &T) {
-  println!("{}", std::any::type_name::<T>())
+	println!("{}", std::any::type_name::<T>())
 }
 
 #[tauri::command]
 async fn insert_dummytest() {
-  let start = SystemTime::now();
-  let since_the_epoch = start
-      .duration_since(UNIX_EPOCH)
-      .expect("Time went backwards");
+	let start = SystemTime::now();
+	let since_the_epoch = start
+			.duration_since(UNIX_EPOCH)
+			.expect("Time went backwards");
 
-  let db = DB.get().unwrap();
-  let collection = db.collection::<Document>("dummytest");
-  let doc = doc! { "time": since_the_epoch.as_secs() as u32 };
-  collection.insert_one(doc, None).await.unwrap();
+	let db = DB.get().unwrap();
+	let collection = db.collection::<Document>("dummytest");
+	let doc = doc! { "time": since_the_epoch.as_secs() as u32 };
+	collection.insert_one(doc, None).await.unwrap();
 }
 
 #[tauri::command]
 async fn get_all_documents() -> Vec<Document> {
-  if DB.get().is_none() { return vec![]; }
+	if DB.get().is_none() { return vec![]; }
 
-  let db = DB.get().unwrap();
-  let collection = db.collection::<Document>(COLLECTION_NAME);
+	let db = DB.get().unwrap();
+	let collection = db.collection::<Document>(COLLECTION_NAME);
 
-  let mut documents: Vec<Document> = vec![];
-  let mut cursor = collection.find(None, None).await.unwrap();
-  while cursor.advance().await.unwrap() {
-    documents.push(cursor.deserialize_current().unwrap());
-  }
-  return documents;
+	let mut documents: Vec<Document> = vec![];
+	let mut cursor = collection.find(None, None).await.unwrap();
+	while cursor.advance().await.unwrap() {
+		documents.push(cursor.deserialize_current().unwrap());
+	}
+	return documents;
 }
 
 #[tokio::main]
 async fn main() {
-  tauri::Builder::default()
-    .invoke_handler(tauri::generate_handler![insert_dummytest, get_all_documents, db_connect])
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+	tauri::Builder::default()
+		.invoke_handler(tauri::generate_handler![insert_dummytest, get_all_documents, db_connect])
+		.run(tauri::generate_context!())
+		.expect("error while running tauri application");
 }
--- src/App.svelte
+++ src/App.svelte
@@ -1,69 +1,69 @@
 <script lang="ts">
-  import { invoke } from '@tauri-apps/api/tauri'
-  import { onMount } from 'svelte'
-  import { SvelteToast, toast } from '@zerodevx/svelte-toast'
-  import Datatable from './lib/Datatable.svelte'
+	import { invoke } from '@tauri-apps/api/tauri'
+	import { onMount } from 'svelte'
+	import { SvelteToast, toast } from '@zerodevx/svelte-toast'
+	import Datatable from './lib/Datatable.svelte'
 
-  let dbConnected = false
-  let dbConnecting = false
-  const dbConnect = async () => {
-    dbConnecting = true
-    try {
-      await invoke("db_connect")
-      dbConnecting = false
-      dbConnected = true
-      toast.push("Successfully connected to database.")
-    } catch(e) {
-      dbConnecting = false
-      dbConnected = false
-      toast.push("Could not connect to database.")
-    }
-  }
-  onMount(() => { dbConnect() })
+	let dbConnected = false
+	let dbConnecting = false
+	const dbConnect = async () => {
+		dbConnecting = true
+		try {
+			await invoke("db_connect")
+			dbConnecting = false
+			dbConnected = true
+			toast.push("Successfully connected to database.")
+		} catch(e) {
+			dbConnecting = false
+			dbConnected = false
+			toast.push("Could not connect to database.")
+		}
+	}
+	onMount(() => { dbConnect() })
 </script>
 
 <svelte:head>
-  <link
-    rel="stylesheet"
-    href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
-  />
+	<link
+		rel="stylesheet"
+		href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+	/>
 </svelte:head>
 
 <main>
-  {#if !dbConnected}
-    <section class="connection">
-      <div class="container">
-        {#if !dbConnecting}
-          Not connected to database. <button class="btn btn-primary btn-sm" on:click={() => { dbConnect() }}>Attempt Connection</button>
-        {:else}
-          Connecting...
-        {/if}
-      </div>
-    </section>
-  {/if}
-  <section class="datatable">
-    <Datatable />
-  </section>
+	{#if !dbConnected}
+		<section class="connection">
+			<div class="container">
+				{#if !dbConnecting}
+					Not connected to database. <button class="btn btn-primary btn-sm" on:click={() => { dbConnect() }}>Attempt Connection</button>
+				{:else}
+					Connecting...
+				{/if}
+			</div>
+		</section>
+	{/if}
+	<section class="datatable">
+		<Datatable />
+	</section>
 </main>
 
 <style>
-  :root {
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
-      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-  }
+	:root {
+		font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+			Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+	}
 
-  section {
-    padding: 1em;
-  }
+	section {
+		padding: 1em;
+	}
 
-  section.connection {
-    background-color: rgba(255, 173, 173, 0.7);
-  }
+	section.connection {
+		background-color: rgba(255, 173, 173, 0.7);
+	}
 
-  section.datatable {
-    min-height: 4em;
-    margin-bottom: 3em;
-  }
+	section.datatable {
+		min-height: 4em;
+		margin-bottom: 3em;
+	}
 </style>
 
 <SvelteToast />
