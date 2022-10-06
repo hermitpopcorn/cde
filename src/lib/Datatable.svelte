522bde5a8ab4e37fcbac6cf765b99ce2f84853be
Dani Nugraha <dani.hmng@gmail.com>
2022-10-06 15:32:01 +0700

    fixed bracket-mark regex bug
    * the old regex ignored marks that only contain one character

--- src/lib/Datatable.svelte
+++ src/lib/Datatable.svelte
@@ -114,7 +114,7 @@
 			const isInBrackets = (str: string): boolean => str.startsWith('[') && str.endsWith(']')
 
 			let finalArray = []
-			string = string.match(/([^[\]]+|(\[(.[^\]]+)\]))/g).map( function(val) { return val==="[]" ? null : val })
+			string = string.match(/(([^[\]]+)|(\[([^\]]+)\]))/g).map((val) => val === "[]" ? null : val)
 			for (let i = 0; i < string.length; i++) {
 				finalArray.push({
 					text: isInBrackets(string[i]) ? string[i].substr(1, string[i].length - 2) : string[i],
